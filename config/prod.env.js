cb94fade881ffec210e600913cc0a3fb195c114b
zw2582 <2582308253@qq.com>
2018-03-19 17:06:26 +0800

    sdf

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: '"production"',
   WS_ADDR:'"ws://120.79.30.72:9502"',	//websocket地址
   WS_REQ:'"http://120.79.30.72:9502"',	//websocket request请求地址
-  WEB_ADDR:'"http://paoma.com"',	//服务端地址
+  WEB_ADDR:'"http://paoma.iillyy.com"',	//服务端地址
 }
